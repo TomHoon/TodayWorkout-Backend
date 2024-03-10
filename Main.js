67f82f5804b34885095a0f24334aa5f3bd44e326
tomhoon <gnsdl9108@naver.com>
2024-03-11 02:22:23 +0900

    feat: 파일업로드 추가

--- Main.js
+++ Main.js
@@ -4,16 +4,40 @@ const app = express();
 const maria = require("./maria.js");
 maria.connect();
 
+const multer = require('multer');
+const upload = multer({
+    storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+      },
+      filename: function (req, file, cb) {
+        file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
+        cb(null, file.originalname);
+      }
+    }),
+  });
+app.post('/uploadFile', upload.single('img'), (req, res) => {
+  console.log('file >>> ', req.file); 
+  console.log('file >>> ', req.body); 
+});
+
+const bodyParser = require("body-parser");
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 const port = process.env.PORT || 3300;
 
 const 마리아 = (query, param) => {
   let result = "";
   maria.query(query, param, function (err, rows) {
     if (err) {
-      console.log("#### error ");
+      console.log(">>> error ");
     } else {
-      console.log("#### success ");
-      console.log("#### rows ", rows);
+      console.log(">>> success ");
+      console.log(">>> rows ", rows);
       result = rows;
     }
   });
@@ -26,6 +50,7 @@ const joinMember = (pw) => {
   return 마리아(query, param);
 };
 
+
 const getMember = (pw) => {
   const query = "SELECT * FROM MEMBER";
   return 마리아(query);
@@ -35,18 +60,45 @@ const getMember = (pw) => {
  * ----------------------------------------
  */
 
-app.get("/members", (req, res) => {
+app.use(express.static('uploads'));
+
+// 멤버조회
+app.get("/getMembers", (req, res) => {
+  console.log(">>> getMember is running");
+
   maria.query("SELECT * FROM MEMBER", function (err, rows) {
     if (err) {
-      console.log("#### error ");
+      console.log(">>> error ");
     } else {
-      console.log("#### success ");
-      console.log("#### rows ", rows[0]);
+      console.log(">>> success ");
+      console.log(">>> rows ", rows[0]);
 
       return res.json(rows[0]);
     }
   });
 });
+
+// 비번업데이트
+app.post("/updatePw", jsonParser, (req, res) => {
+  console.log(">>> updateMember is running");
+  console.log("req.body >>> ", req.body);
+
+  let { member_id, member_pw } = req.body;
+  let query = `UPDATE MEMBER SET member_pw =? WHERE member_id = ?`;
+  let param = [member_pw, member_id];
+  maria.query(query, param, function (err, rows) {
+    if (err) {
+      console.log(">>> error ");
+    } else {
+      console.log(">>> success ");
+      console.log(">>> rows ", rows[0]);
+
+      return res.json(rows[0]);
+    }
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`server is listening at localhost:${process.env.PORT}`);
--- maria.js
+++ maria.js
@@ -1,8 +1,8 @@
 const maria = require('mysql');
 
 const conn = maria.createConnection({
-    // host : '192.168.0.100',
-    host : 'localhost',
+    host : '192.168.0.100',
+    // host : 'localhost',
     port : 13306,
     user:'root',
     password:'1234',
