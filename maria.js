c03a26e6219c46b4d24b4e091fcc79d46c7034fc
남병현 <kmi5063@naver.com>
2024-03-14 13:22:51 +0900

    fix : hostAddr 수정

--- maria.js
+++ maria.js
@@ -1,13 +1,13 @@
 const maria = require('mysql');
 
 const hostAddr = {
-    운영: 'tomhoon.duckdns.org:13300',
+    운영: 'tomhoon.duckdns.org',
     내부망: '192.168.0.100', // tomhoon만
     개발: 'localhost',
 }
 
 const conn = maria.createConnection({
-    host : hostAddr.운영,  
+    host : hostAddr.운영,
     port : 13306,
     user:'root',
     password:'1234',
